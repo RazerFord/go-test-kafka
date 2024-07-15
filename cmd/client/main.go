a424b663eb4d467cebe95e0a31199e0456653f6f
RazerFord <desk10567@gmail.com>
2024-07-15 13:08:14 +0300

    added consumer and producer

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"context"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, i'm client")
-	})
+const topic = "test-topic"
 
-	http.ListenAndServe(":8081", nil)
+func main() {
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:29092", topic, partition)
+
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("one!")},
+		kafka.Message{Value: []byte("two!")},
+		kafka.Message{Value: []byte("three!")},
+	)
+
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, i'm server")
-	})
+const topic = "test-topic"
 
-	http.ListenAndServe(":8082", nil)
+func main() {
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:29092", topic, partition)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
+
+	b := make([]byte, 10e3) // 10KB max per message
+	for {
+		n, err := batch.Read(b)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(b[:n]))
+	}
+
+	if err := batch.Close(); err != nil {
+		log.Fatal("failed to close batch:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close connection:", err)
+	}
 }
