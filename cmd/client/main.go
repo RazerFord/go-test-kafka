9635ac01e7faf5fd3c549d62a729f61fa088e6aa
RazerFord <desk10567@gmail.com>
2024-07-18 11:55:38 +0300

    done docker-compose

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"github.com/segmentio/kafka-go"
 )
 
 const topic = "test-topic"
+const host = "kafka:9092"
 
 func main() {
+	log.SetOutput(os.Stdout)
+	
 	partition := 0
 
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:29092", topic, partition)
+	log.Println(host)
+	conn, err := kafka.DialLeader(context.Background(), "tcp", host, topic, partition)
 
 	if err != nil {
 		log.Fatal("failed to dial leader:", err)
